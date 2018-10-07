4586f303a50b96412d33d0a5d65f84f51db9c752
Vaqcoders <42900934+vaqcoders@users.noreply.github.com>
2018-10-07 10:41:10 -0700

    !iwannabetheverybest command
    Responds with "LIKE NO ONE EVER WAS!!!" with a smiley emoji.

--- bot.js
+++ bot.js
@@ -3,43 +3,57 @@
   Discord.js Docs: https://discord.js.org/#/docs/main/stable/general/welcome
 */
 
-const Discord = require("discord.js"),
-      bot = new Discord.Client({autorun: true});
+const Discord = require("discord.js");
+const bot = new Discord.Client({autorun: true});
 
 const emoji = require("emoji.json");
-
 
 bot.on("ready", () => {
   console.log("I am ready!");
 });
 
 bot.on("message", msg => {
+  
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
   if (msg.content.substring(0, 1) == "!") {
+    
     let args = msg.content.substring(1).split(" ");
+    
     switch(args[0]) {
+        
       case "vaq":
         msg.channel.send("Coders!");
         break;
+        
       case "greet":
         msg.channel.send(`Hello, ${msg.author.username}!`);
         break;
+        
       case "love":
         msg.channel.send(":heart:");
         break;
+        
       case "bye":
         msg.channel.send("rip... :broken_heart:");
         break;
+        
       case "wah":
         msg.channel.send("I'm sorry... :cry:");
         break;
+        
+      case "iwannabetheverybest":
+        msg.channel.send("LIKE NO ONE EVER WAS!!!:smiley:");
+        break;
+        
       case "food":
         let gimmePossibleFoods = getEmoji("food");
         let gimmeFood = gimmePossibleFoods[Math.floor(Math.random() * gimmePossibleFoods.length)];
         msg.channel.send(`Here's some ${gimmeFood.name}, ${msg.author.username}. ${emoji[366].char} ${gimmeFood.char}`);
         break;
+        
       // Just add any case commands if you want to..
+        
     }
   }
 });
